9b5224f902fe05392a068c426ae1d2c0259561a9
pajubrat <pauli.j.brattico@gmail.com>
2025-05-27 17:58:11 +0200

    Added basic functionality for modelling several languages

--- template3_1.py
+++ template3_1.py
@@ -6,51 +6,10 @@
 
 import itertools
 import sys
-
-# Defines a simple root lexicon as a dictionary
-lexicon = {'a': {'a'}, 'b': {'b'}, 'c': {'c'}, 'd': {'d'},
-           'the': {'D'},
-           'dog': {'N'},
-           'bark': {'V', 'V/INTR'},
-           'barks': {'V', 'V/INTR'},
-           'bites*': {'V', '+COMP:D', '+SPEC:D'},
-           'bite': {'V', 'V/TR'},
-           'bites': {'V', 'V/TR'},
-           'bite*': {'V', 'V/TR'},
-           'which': {'D', 'WH', 'OP'},
-           'which*': {'D', 'REL', 'OP', 'PC:#X', '+wCOMP:N0'},
-           'man': {'N'},
-           'N0': {'N', 'N0'},
-           'angry': {'A', 'α:N'},
-           'frequently': {'Adv'},
-           'city': {'N'},
-           'from': {'P'},
-           'in': {'P', 'α:V'},
-           'ed': {'T', 'PC:#X', '+wCOMP:V'},
-           'T': {'T', 'PC:#X', 'EPP', '+wCOMP:V'},
-           'did': {'T', 'EPP', 'AUX'},
-           'does': {'T', 'AUX'},
-           'was': {'T', 'EPP', 'AUX'},
-           'C': {'C'},
-           'C(wh)': {'C', 'C(wh)', 'PC:#X', '+wCOMP:T', 'OP', 'WH', 'SCOPE'},
-           'C(rel)': {'C', 'C(rel)', 'REL', 'SCOPE', 'OP', 'α:N', 'λ:R', 'PHON:0'},
-           'v': {'v', 'PC:#X', '+wCOMP:V'}}
-
-# Lexical redundancy rules add features to lexical items based on their feature content
-# This creates speaker lexicons
-lexical_redundancy_rules = {'D': {'+COMP:N'},
-                            'Adv': {'+SPEC:Adv,ø', 'λ:R', 'α:V'},
-                            'P': {'+COMP:D', 'λ:R'},
-                            'C': {'+COMP:T', '+SPEC:D,ø'},
-                            'A': {'+COMP:N,ø', 'λ:L', 'α:N'},
-                            'N': {'+COMP:C,P,ø,C(rel)', '+SPEC:A,ø'},
-                            'T': {'+COMP:V', '+SPEC:D'},
-                            'v': {'V', '+COMP:V', '+SPEC:D', '+wCOMP:V', '-ε'},
-                            'V/INTR': {'+SPEC:D'},
-                            'V/TR': {'+COMP:D'}
-                            }
-
+import copy
 major_lexical_categories = ['C', 'N', 'v', 'V', 'T/inf', 'A', 'D', 'Adv', 'T', 'P', 'a', 'b', 'c', 'd']
+
+log_file = open('log.txt', 'w', encoding='utf-8')
 
 # Auxiliary functions used during certain processing steps
 def tcopy(SO):
@@ -75,20 +34,40 @@ def print_constituent_lst(sWM):
     return str
 
 
-class Lexicon:
+class SpeakerLexicon:
     """Stores lexical knowledge independent of the syntactic phrase structure"""
-    def __init__(self):
-        self.speaker_lexicon = dict()   # The lexicon is a dictionary
-        self.compose_speaker_lexicon()  # Creates the runtime lexicon by combining the root lexicon and
-                                        # the lexical redundancy rules
+    def __init__(self, L, ld):
+        self.speaker_lexicon = dict()                       # Speaker lexicon, as dictionary
+        self.create_speaker_lexicon(L, ld)     # Create speaker lexicon
+        self.apply_redundancy_rules(ld.root_lexical_redundancy_rules)
 
-    def compose_speaker_lexicon(self):
+    def create_speaker_lexicon(self, L, ld):
+        """Creates a speaker lexicon by applying redundancy rules"""
+
+        for lex in ld.root_lexicon.keys():
+            features = set(ld.root_lexicon[lex])
+            if self.target_language(features, L):
+                self.speaker_lexicon[lex] = features
+                self.speaker_lexicon[lex].add(L)
+
+    def target_language(self, features, L):
+        """
+        Determines whether lexical features 'features' belong to lexical item in language 'language'.
+        It does if there is no contradictory language feature; feature sets with no language specification
+        are assumed to be universal and belong to all languages
+        """
+        for f in features:
+            if f.startswith('LANG:') and f != L:
+                return False
+        return True
+
+    def apply_redundancy_rules(self, root_lexical_redundancy_rules):
         """Composes the speaker lexicon from the list of words and lexical redundancy rules"""
-        for lex in lexicon.keys():
-            self.speaker_lexicon[lex] = lexicon[lex]
-            for trigger_feature in lexical_redundancy_rules.keys():
-                if trigger_feature in lexicon[lex]:
-                    self.speaker_lexicon[lex] = self.speaker_lexicon[lex] | lexical_redundancy_rules[trigger_feature]
+        for trigger_features_str in root_lexical_redundancy_rules.keys():
+            trigger_features = set(trigger_features_str.strip().split(' '))
+            for lex in self.speaker_lexicon.keys():
+                if trigger_features <= self.speaker_lexicon[lex]:
+                    self.speaker_lexicon[lex] = self.speaker_lexicon[lex] | root_lexical_redundancy_rules[trigger_features_str]
 
     def retrieve(self, name):
         """Retrieves lexical items from the speaker lexicon and wraps them
@@ -107,14 +86,14 @@ class Lexicon:
     def __str__(self):
         stri = ''
         for lex in self.speaker_lexicon.keys():
-            stri += f'{lex} {" ".join([f for f in list(self.speaker_lexicon[lex])])}\n'
+            stri += f'{lex} = {" ".join([f for f in sorted(list(self.speaker_lexicon[lex]))])}; '
         return stri
 
 class PhraseStructure:
     """Simple asymmetric binary-branching bare phrase structure formalism"""
-    logging = None          # class variable: whether logging is on or off, affects calculation speed
-    chain_index = 1         # class variable: creates chain indexes
-    logging_report = ''     # class variable: used to store logging information during the derivation
+    logging = None              # class variable: whether logging is on or off, affects calculation speed
+    chain_index = 1             # class variable: creates chain indexes
+    logging_report = ''         # class variable: used to store logging information during the derivation
     logging_report_detailed = ''
     def __init__(self, X=None, Y=None):
         self.const = (X, Y)             # Constituents are represented as tuples
@@ -345,6 +324,14 @@ class PhraseStructure:
             PhraseStructure.chain_index += 1
             X.chain_index = PhraseStructure.chain_index
 
+    def external_search(X, fset):
+        Origin = X
+        while X:
+            for x in (X,) + X.const:
+                if x and x != Origin and fset <= x.features:
+                    return x
+            X = X.mother
+
     def internal_search(X, feature):
         """One implementation for minial search"""
         while X:
@@ -407,9 +394,9 @@ class PhraseStructure:
 
     def complement_subcategorization(X, Y):
         """Complement subcategorization under [X Y]"""
-        if (not Y and (not X.selected_features('+COMP:') or 'ø' in X.selected_features('+COMP:'))) or \
-                (Y and X.selected_features('+COMP:') and
-                 Y.head().check(X.selected_features('+COMP:')) and
+        if (not Y and (not X.selected_features('COMP:') or 'ø' in X.selected_features('COMP:'))) or \
+                (Y and X.selected_features('COMP:') and
+                 Y.head().check(X.selected_features('COMP:')) and
                  Y.head().subcategorization()):
             return True
         PhraseStructure.logging_report_detailed += f'\n\t*Complement selection violation by |{X}|° ({X.get_selection_features()})'
@@ -418,7 +405,7 @@ class PhraseStructure:
         """Specifier subcategorization under [XP YP]"""
         if not Spec:
             if not X.specifier():
-                if X.selected_features('+SPEC:') and 'ø' not in X.selected_features('+SPEC:'):
+                if X.selected_features('SPEC:') and 'ø' not in X.selected_features('SPEC:'):
                     PhraseStructure.logging_report_detailed += f'\n\t*Missing specifier selection violation by |{X}|° ({X.get_selection_features()})'
                     return False
                 return True
@@ -430,13 +417,32 @@ class PhraseStructure:
             PhraseStructure.logging_report_detailed += f'\n\t*Multiple  specifier selection violation by |{X}|° ({X.get_selection_features()})'
             return False
 
-        if not X.selected_features('+SPEC:') or not Spec.head().check(X.selected_features('+SPEC:')):
+        if not X.selected_features('SPEC:') or not Spec.head().check(X.selected_features('SPEC:')):
             PhraseStructure.logging_report_detailed += f'\n\t*Positive specifier selection violation by |{X}|° ({X.get_selection_features()}) against {Spec}'
             return False
         return True
 
     def check(X, features):
         return {f for f in features if f != 'ø'} & X.features
+
+    def case_checking(X):
+        # Recursion
+        if not X.zero_level():
+            return X.left().case_checking() and X.right().case_checking()
+        # Either (i) X is not case assignee or it is and
+        # (ii) we have unmarked (NOM) option or
+        # (iii) there is a suitable case assigner (κ-head)
+        if not X.case_assignee_features() or \
+                (not X.external_search({'κ'}) and 'ø' in X.case_assignee_features()) or \
+                (X.external_search({'κ'}) and X.external_search({'κ'}).features & X.case_assignee_features()):
+            return True
+        # ... otherwise checking fails.
+        PhraseStructure.logging_report_detailed += f'\n\t*|{X}| failed case checking for {X.case_assignee_features()}'
+
+    def case_assignee_features(X):
+        for f in X.features:
+            if ':' in f and f.split(':')[0] == 'κ':
+                return set(f.split(':')[1].split(','))
 
     def selected_features(X, type):
         s = set()
@@ -510,7 +516,7 @@ class PhraseStructure:
         return 'λ:R' in X.head().features
 
     def get_selection_features(X):
-        return ' '.join([x for x in X.features if x.startswith('+COMP:') or x.startswith('+SPEC:')])
+        return ' '.join([x for x in X.features if x.startswith('COMP:') or x.startswith('SPEC:')])
 
     def isRoot(X):
         """Roots are constituents that do not have mothers"""
@@ -527,7 +533,7 @@ class PhraseStructure:
 
     def __str__(X):
         """Simple printout function for phrase structure objects"""
-        if X.elliptic:
+        if X.elliptic and not X.zero:
             return '__' + X.get_chain_subscript()
         if X.terminal():
             return X.phonological_exponent
@@ -548,20 +554,20 @@ class PhraseStructure:
 # Model of the speaker which constitutes the executive layer
 # In more realistic models the speaker models must be language-specific
 #
-class SpeakerModel:
-    def __init__(self):
+class SpeakerModel():
+    def __init__(self, ld, language):
         # List of all syntactic operations available in the grammar
         self.syntactic_operations = [(PhraseStructure.MergePreconditions, PhraseStructure.MergeComposite, 2, 'Merge'),
                                      (PhraseStructure.HeadMergePreconditions, PhraseStructure.HeadMerge_, 2, 'Head Merge'),
                                      (PhraseStructure.AdjunctionPreconditions, PhraseStructure.Adjoin_, 2, 'Adjoin')]
-        self.n_accepted = 0         # Number of accepted sentences
-        self.n_steps = 0            # Number of steps consumed
-        self.output_data = set()    # A set containing all output data
-        self.lexicon = Lexicon()    # Lexicon
-        self.log_file = None        # Logging
+        self.n_accepted = 0                             # Number of accepted sentences
+        self.n_steps = 0                                # Number of steps consumed
+        self.output_data = set()                        # A set containing all output data
+        self.lexicon = SpeakerLexicon(language, ld)         # Lexicon
+        self.language = language
 
     def derive(self, numeration):
-        """Finds all derivations from numeraton"""
+        """Finds all derivations from numeration"""
         self.n_steps = 0
         self.output_data = set()
         self.n_accepted = 0
@@ -579,7 +585,10 @@ class SpeakerModel:
                         new_sWM = {x for x in sWM if x not in set(SO)} | tset(OP(*tcopy(SO)))
                         self.consume_resource(new_sWM, sWM)
                         self.derivational_search_function(new_sWM)
-            self.log_file.write('.')
+            log_file.write('.')
+
+    def __str__(self):
+        return f'\nSpeaker Model {self.language}:\n{self.lexicon}\n'
 
     @staticmethod
     def derivation_is_complete(sWM):
@@ -595,9 +604,9 @@ class SpeakerModel:
         Modify to enhance readability and to reflect the operations available in the grammar
         """
         self.n_steps += 1
-        self.log_file.write(f'\n\n{self.n_steps}.\t{{ {print_constituent_lst(old_sWM)} }}\n')
-        self.log_file.write(f'{PhraseStructure.logging_report} =')
-        self.log_file.write(f'\n\t{{ {print_constituent_lst(new_sWM)} }}')
+        log_file.write(f'\n\n{self.n_steps}.\t{{ {print_constituent_lst(old_sWM)} }}\n')
+        log_file.write(f'{PhraseStructure.logging_report} =')
+        log_file.write(f'\n\t{{ {print_constituent_lst(new_sWM)} }}')
         PhraseStructure.logging_report = ''
         PhraseStructure.logging_report_detailed = ''
 
@@ -605,22 +614,56 @@ class SpeakerModel:
         PhraseStructure.chain_index = 0
         PhraseStructure.logging_report_detailed = ''
         for X in sWM:
-            if not X.subcategorization() or not X.operator_variable_condition():
-                self.log_file.write(f'{PhraseStructure.logging_report_detailed}')
+            if not X.subcategorization() or not X.operator_variable_condition() or not X.case_checking():
+                log_file.write(f'{PhraseStructure.logging_report_detailed}')
                 return
+
         self.n_accepted += 1
         prefix = f'{self.n_accepted}'
         output_sentence = f'{self.root_structure(sWM).linearize()}'
         print(f'\n({prefix}.)\n{output_sentence}\n{print_constituent_lst(sWM)}\n')   # Print the output
-        self.log_file.write(f'\n=>\t/{output_sentence[:-1]}/ accepted.')
+        log_file.write(f'\n=>\t/{output_sentence[:-1]}/ accepted.')
         self.output_data.add(output_sentence.strip())
-        self.log_file.write('\n')
+        log_file.write('\n')
+
 
 class LanguageData:
-    """Stores and manipulates all data used in the simulation"""
-    def __init__(self):
+    """Stores and manipulates all data used in the simulation, including logging output"""
+    def __init__(self, filename):
         self.study_dataset = []     # dataset is a list of (numeration, set of target sentences) tuples
-        self.log_file = None
+        self.root_lexicon = dict()                       # Root lexicon contains the whole lexicon from the file
+        self.root_lexical_redundancy_rules = dict()      # Root redundancy rules
+        self.read_root_lexicon(filename)        # Read the root lexicon
+        self.languages = self.set_languages()
+
+    def read_root_lexicon(self, filename):
+        """Reads the root lexicon (all lexical items and redundancy rules)"""
+        with open(filename, encoding='utf-8') as f:
+            for line in f.readlines():
+                line = line.strip()
+                if not line.startswith('#'):
+                    if '::' in line:
+                        key, features_str = line.split('::')
+                        features = set(features_str.strip().split(' '))
+                        self.root_lexicon[key.strip()] = features
+                    if '=>' in line:
+                        trigger, features = line.split('=>')
+                        self.root_lexical_redundancy_rules[trigger.strip()] = set(features.strip().split(' '))
+
+    def set_languages(self):
+        Ls = set()
+        for key in self.root_lexicon:
+            for f in self.root_lexicon[key]:
+                if f.startswith('LANG:'):
+                    Ls.add(f)
+        return Ls
+
+    def guess_language(self, numeration):
+        for x in numeration:
+            if x in self.root_lexicon.keys():
+                for f in self.root_lexicon[x]:
+                    if f.startswith('LANG:'):
+                        return f
 
     # Read the dataset
     def read_dataset(self, filename):
@@ -643,11 +686,11 @@ class LanguageData:
                     break
             self.study_dataset.append((numeration, dataset))
 
-    def start_logging(self):
-        log_file = 'log.txt'
-        log_file = open(log_file, 'w', encoding='utf-8')
-        PhraseStructure.logging = log_file
-        return log_file
+    def print_root_lexicon(self):
+        stri = 'ROOT LEXICON:\n'
+        for key in self.root_lexicon.keys():
+            stri += f'{key}: {self.root_lexicon[key]}\n'
+        return stri + '\n\n'
 
     def evaluate_experiment(self, output_from_simulation, gold_standard_dataset, n_steps):
         print(f'\tDerivational steps: {n_steps}')
@@ -663,27 +706,31 @@ class LanguageData:
 
 # Run one whole study as defined by the dataset file, itself containing
 # numeration-target sentences blocks
-def run_study(ld, sm):
-    sm.log_file = ld.start_logging()
+def run_study(ld, speaker_models):
     n_dataset = 0       #   Number of datasets in the experiment (counter)
     n_total_errors = 0  #   Count the number of errors in the whole experiment (counter)
     n_total_steps = 0   #   Number of calculations steps in the whole experiment
-    sm.log_file.write(f'{sm.lexicon}')
     for numeration, gold_standard_dataset in ld.study_dataset:
         n_dataset += 1
-        print(f'Dataset {n_dataset} ({numeration}):')
-        sm.log_file.write('\n---------------------------------------------------\n')
-        sm.log_file.write(f'Dataset {n_dataset}:\n')
-        sm.log_file.write(f'Numeration: {numeration}\n')
-        sm.log_file.write(f'Predicted outcome: {gold_standard_dataset}')
-        sm.derive(numeration)
-        n_total_errors += ld.evaluate_experiment(sm.output_data, gold_standard_dataset, sm.n_steps)
-        n_total_steps += sm.n_steps
+        L = ld.guess_language(numeration)
+        print(f'Dataset {n_dataset} ({numeration}, {L}):')
+        log_file.write('\n---------------------------------------------------\n')
+        log_file.write(f'Dataset {n_dataset}:\n')
+        log_file.write(f'Numeration: {numeration}, {L}\n')
+        log_file.write(f'Predicted outcome: {gold_standard_dataset}')
+
+        speaker_models[L].derive(numeration)
+
+        n_total_errors += ld.evaluate_experiment(speaker_models[L].output_data, gold_standard_dataset, speaker_models[L].n_steps)
+        n_total_steps += speaker_models[L].n_steps
     print(f'\nTotal errors {n_total_errors} after {n_total_steps} calculation steps.\n')
-    sm.log_file.write(f'\nTotal errors: {n_total_errors}')
+    log_file.write(f'\nTotal errors: {n_total_errors}')
 
-
-ld = LanguageData()                         #   Instantiate the language data object
-ld.read_dataset('dataset_template3_1.txt')    #   Name of the dataset file processed by the script, reads the file
-sm = SpeakerModel()                         #   Create default speaker model, would be language-specific in a more realistic model
-run_study(ld, sm)                           #   Runs the study
+ld = LanguageData('lexicon3_1.txt')             #   Instantiate language data object, including root lexicons
+ld.read_dataset('dataset_template3_1.txt')      #   Name of the dataset file processed by the script, reads the file
+speaker_models = {}
+log_file.write(f'Root lexicon: {ld.root_lexicon}\n')
+for language in ld.languages:
+    speaker_models[language] = SpeakerModel(ld, language=language)
+    log_file.write(f'Speaker Model {language} lexicon: {speaker_models[language].lexicon}\n')
+run_study(ld, speaker_models)
